9f52bfafd131b3defea35c6883eccc38d1323d83
Charles Ivia <iviacharles@gmail.com>
2020-09-01 16:21:26 +0300

    CreateTodo from

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const CreateTodo = () => {
+  const { register, handleSubmit } = useForm();
+
+
   return (
     <div className="container">
-      <h1>Create Todo</h1>
+      <div className="mt-3">
+        <h3>Create Todo Item</h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text">Text</label>
+            <input
+              ref={register}
+              className="form-control"
+              type="text"
+              name="text"
+              id="text"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Create Todo
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
