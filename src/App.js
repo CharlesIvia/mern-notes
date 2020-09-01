7e196ee657d46c20604d1c4a5e921acd984fbdc9
Charles Ivia <iviacharles@gmail.com>
2020-09-01 15:57:25 +0300

    Navbar and Links

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar bg-ligt navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link className="nav-link" to="/">
+              Todos
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link className="nav-link" to="create">
+              Create Todo
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={TodoList} />
+        <Route exact path="/edit:id" component={EditTodo} />
+        <Route exact path="/create" component={CreateTodo} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
