806e2ea7b47081b749241416b3efbbfd6fa7964d
Charles Ivia <iviacharles@gmail.com>
2020-09-01 19:29:07 +0300

    Edit todo

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,27 @@ app.get("/", (req, res) => {
     });
 });
 
+//
+
+app.post("/:id", (req, res) => {
+  const id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    if (!todo) {
+      res.status(404).send("Todo not found");
+    } else {
+      todo.text = req.body.text;
+      todo
+        .save()
+        .then((todo) => {
+          res.json(todo);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+});
+
 //Get Specific
 
 app.get("/:id", (req, res) => {
