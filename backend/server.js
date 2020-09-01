a9b03b63330e204b76a0e6b301082e34319889f8
Charles Ivia <iviacharles@gmail.com>
2020-09-01 17:31:47 +0300

    Connected MongoDb database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const dbURL =
+  "mongodb+srv://gatsby:gatsby2020@cluster0.holkp.mongodb.net/node-blog?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 const PORT = 4000;
 const app = express();
@@ -8,5 +20,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log("Server is running on port" + PORT);
 });
