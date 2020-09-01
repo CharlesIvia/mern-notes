acaa4c0583ba7477c55a4bb0cc6520cca0024057
Charles Ivia <iviacharles@gmail.com>
2020-09-01 17:18:42 +0300

    Formatted component

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,40 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 
-const TodoForm = ({ todo }) => {
+const TodoForm = ({ todo, onSubmit }) => {
   const { register, handleSubmit } = useForm({
     defaultValues: { text: todo ? todo.text : "" },
   });
 
   const history = useHistory();
 
-  const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data));
+  const onSubmitHandler = handleSubmit((data) => {
+    onSubmit(data);
     history.push("/");
   });
   return (
-    <div className="container">
-      <div className="mt-3">
-        <h3>Edit Todo Item</h3>
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="text">Text</label>
-            <input
-              ref={register}
-              className="form-control"
-              type="text"
-              name="text"
-              id="text"
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-primary">
-              Edit Todo
-            </button>
-          </div>
-        </form>
-      </div>
+    <div>
+      {" "}
+      <form onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <label htmlFor="text">Text</label>
+          <input
+            ref={register}
+            className="form-control"
+            type="text"
+            name="text"
+            id="text"
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Save Todo
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
