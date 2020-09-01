e9c1db367ef05af926d33ad8638ea0d7e56e8700
Charles Ivia <iviacharles@gmail.com>
2020-09-01 17:38:24 +0300

    Get todos

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const PORT = 4000;
+const app = express();
 
+//Connect to Db
 const dbURL =
   "mongodb+srv://gatsby:gatsby2020@cluster0.holkp.mongodb.net/node-blog?retryWrites=true&w=majority";
 
@@ -13,12 +17,22 @@ mongoose
   .then(console.log("DB Connected"))
   .catch((err) => console.log(err));
 
-const PORT = 4000;
-const app = express();
+//Middleware
 
 app.use(cors());
 app.use(express.json());
 
+//Routes
+app.get("/", (req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+//Listen
 app.listen(PORT, () => {
   console.log("Server is running on port" + PORT);
 });
